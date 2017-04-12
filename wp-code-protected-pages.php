19be10e15bebddd50b19caa45944b43c676643ba
hayzem <aliatasever@gmail.com>
2017-04-13 00:50:28 +0300

    update class, remove global methods, add used field for entity

--- wp-code-protected-pages.php
+++ wp-code-protected-pages.php
@@ -10,196 +10,32 @@
  GitHub Plugin URI: https://github.com/hayzem/wp-code-protected-pages
  */
 
-/**
- * @todo: create class for all methods
- */
-
-add_action( 'template_redirect', 'update_passwords' );
-
-function update_passwords()
-{
-    global $post;
-    $global = false;
-
-    if ( empty( $post ) && isset( $GLOBALS['post'] ) )
-    {
-        $post = $GLOBALS['post'];
-        $global = true;
-    }
-
-    if( ! $post )
-        return null;
-
-    $correct_password = is_array( $post ) ? $post['post_password'] : $post->post_password;
-
-    if( ! $correct_password )
-        return null;
-
-    if ( ! isset( $_COOKIE['wp-postpass_' . COOKIEHASH ] ) )
-        return;
-
-    if( ! class_exists( 'PasswordHash' ) )
-        require_once ABSPATH . WPINC . '/class-phpass.php';
-
-    $hasher = new PasswordHash( 8, true );
-    $hash = wp_unslash( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] );
-    if ( 0 !== strpos( $hash, '$P$B' ) )
-        return;
-
-    $passed = false;
-
-
-    $entities = getEntities();
-    foreach( $entities as $entity ){
-        if( ! $entity->code )
-            continue;
-
-        $check_password = $entity->code;
-
-        if( is_string( $check_password ) || is_numeric( $check_password ) )
-            $check_password = trim( $check_password );
-        else
-            continue;
-
-        if( $hasher->CheckPassword( $check_password, $hash ) ){
-            $passed = $check_password;
-            insertLog($entity, 'LOGGED_IN');
-            break;
-        }
-    }
-
-    if( ! $passed )
-        return;
-
-    if( is_array( $post ) ){
-        $post['post_password'] = $passed;
-    } else {
-        $post->post_password = $passed;
-    }
-
-    if( $global )
-        $GLOBALS['post'] = $post;
-}
-
-function createPluginDatabaseTable()
-{
-    global $table_prefix, $wpdb;
-
-    $charset_collate = $wpdb->get_charset_collate();
-    $tableName = 'wcpp';
-    $wp_table = $table_prefix . "$tableName";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-    if($wpdb->get_var( "show tables like '$wp_table'" ) != $wp_table) {
-        $sql = "CREATE TABLE $wp_table (
-              id mediumint(9) NOT NULL AUTO_INCREMENT,
-              code tinytext NOT NULL,
-              name text NOT NULL,
-              description text NOT NULL,
-              created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-              PRIMARY KEY  (id)
-            ) $charset_collate;
-        ";
-
-        dbDelta($sql);
-    }
-
-    $logTableName = 'wcpp_log';
-    $wp_log_table = $table_prefix . "$logTableName";
-
-    if($wpdb->get_var( "show tables like '$wp_log_table'" ) != $wp_log_table) {
-        $sql = "CREATE TABLE $wp_log_table (
-              id mediumint(9) NOT NULL AUTO_INCREMENT,
-              entity_id integer NOT NULL,
-              title text NOT NULL,
-              details text NOT NULL,
-              created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-              PRIMARY KEY  (id)
-            ) $charset_collate;
-        ";
-
-        dbDelta($sql);
-    }
-}
-
-register_activation_hook( __FILE__, 'createPluginDatabaseTable' );
-
-/**
- * @param $entity
- */
-function insertEntity($entity)
-{
-    global $table_prefix, $wpdb;
-
-    $tableName = 'wcpp';
-    $wp_track_table = $table_prefix . "$tableName";
-
-    $wpdb->insert(
-        $wp_track_table,
-        array(
-            'code' => $entity['code'],
-            'name' => $entity['name'],
-            'description' => $entity['description'],
-            'created_at' => current_time( 'mysql' ),
-        )
-    );
-}
-
-function insertLog($entity, $action = 'ACTION_UNKNOWN')
-{
-    global $table_prefix, $wpdb;
-
-    $logTableName = 'wcpp_log';
-    $wp_log_table = $table_prefix . "$logTableName";
-
-    $wpdb->insert(
-        $wp_log_table,
-        array(
-            'entity_id' => $entity->id,
-            'title' => $entity->name.' '.$action,
-            'details' => $entity->name.' '.$entity->description,
-            'created_at' => current_time( 'mysql' ),
-        )
-    );
-}
-
-function getEntities(array $options = [])
-{
-    global $wpdb,$table_prefix;
-    $tableName = 'wcpp';
-    $wp_track_table = $table_prefix . "$tableName";
-
-    return $wpdb->get_results( 'SELECT * FROM '.$wp_track_table.';', OBJECT );
-}
-
-
 class WPCodeProtectedPages
 {
-
-    private $screenName;
+    private $screenName = 'WP Code Protected Pages';
     private static $instance;
     private $_wpdb;
+    private $_post;
     private $_tablePrefix;
     private $_tableName = 'wcpp';
     private $_tableNameLog = 'wcpp_log';
 
     public function __construct()
     {
-        global $table_prefix, $wpdb;
+        global $table_prefix, $wpdb, $post;
 
         $this->_wpdb = $wpdb;
+        $this->_post = $post;
         $this->_tablePrefix = $table_prefix;
-
     }
 
     static function getInstance()
     {
-
         if (!isset(self::$instance))
         {
             self::$instance = new self();
         }
+
         return self::$instance;
     }
 
@@ -216,6 +52,11 @@ class WPCodeProtectedPages
         ?>
         <div class='wrap'>
             <h2>Passcode Dashboard</h2>
+            <ul>
+                <li>Create New Code</li>
+                <li>List of Codes</li>
+                <li>Logs</li>
+            </ul>
         </div>
         <?php
     }
@@ -373,7 +214,7 @@ class WPCodeProtectedPages
     /**
      * @param $entity
      */
-    function insertEntity($entity)
+    public function insertEntity($entity)
     {
         $wp_track_table = $this->_tablePrefix . "$this->_tableName";
 
@@ -385,6 +226,31 @@ class WPCodeProtectedPages
                 'description' => $entity['description'],
                 'created_at' => current_time( 'mysql' ),
             ]
+        );
+    }
+
+    public function updateEntity($entity)
+    {
+        $wp_track_table = $this->_tablePrefix . "$this->_tableName";
+
+        $this->_wpdb->update(
+            $wp_track_table,
+            [
+                'code' => $entity->code,
+                'name' => $entity->name,
+                'description' => $entity->description,
+                'used' => $entity->used,
+            ],
+            [
+                'id' => $entity->id
+            ],
+            [
+                '%s',
+                '%s',
+                '%s',
+                '%d',
+            ],
+            ['%d']
         );
     }
 
@@ -402,10 +268,133 @@ class WPCodeProtectedPages
         return $this->_wpdb->get_results( 'SELECT * FROM '.$wp_track_table.';', OBJECT );
     }
 
+    public function createPluginTables()
+    {
+        $charset_collate = $this->_wpdb->get_charset_collate();
+        $wp_table = $this->_tablePrefix . "$this->_tableName";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        if($this->_wpdb->get_var( "show tables like '$wp_table'" ) != $wp_table) {
+            $sql = "CREATE TABLE $wp_table (
+              id mediumint(9) NOT NULL AUTO_INCREMENT,
+              code tinytext NOT NULL,
+              name text NOT NULL,
+              description text NOT NULL,
+              used integer NOT NULL DEFAULT 0,
+              created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+              PRIMARY KEY  (id)
+            ) $charset_collate;
+        ";
+
+            dbDelta($sql);
+        }
+
+        $wp_log_table = $this->_tablePrefix . "$this->_tableNameLog";
+
+        if($this->_wpdb->get_var( "show tables like '$wp_log_table'" ) != $wp_log_table) {
+            $sql = "CREATE TABLE $wp_log_table (
+              id mediumint(9) NOT NULL AUTO_INCREMENT,
+              entity_id integer NOT NULL,
+              title text NOT NULL,
+              details text NOT NULL,
+              created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+              PRIMARY KEY  (id)
+            ) $charset_collate;
+        ";
+
+            dbDelta($sql);
+        }
+    }
+
+    function updatePasswords()
+    {
+        $global = false;
+        $post = $this->_post;
+
+        if (empty($this->_post) && isset($GLOBALS['post']))
+        {
+            $post = $GLOBALS['post'];
+            $global = true;
+        }
+
+        if( ! $post )
+            return null;
+
+        $correct_password = is_array( $post ) ? $post['post_password'] : $post->post_password;
+
+        if( ! $correct_password )
+            return null;
+
+        if ( ! isset( $_COOKIE['wp-postpass_' . COOKIEHASH ] ) )
+            return;
+
+        if( ! class_exists( 'PasswordHash' ) )
+            require_once ABSPATH . WPINC . '/class-phpass.php';
+
+        $hasher = new PasswordHash( 8, true );
+        $hash = wp_unslash( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] );
+        if ( 0 !== strpos( $hash, '$P$B' ) )
+            return;
+
+        $passed = false;
+
+        $entities = $this->getEntities();
+        foreach( $entities as $entity ){
+            if( ! $entity->code )
+                continue;
+
+            $check_password = $entity->code;
+
+            if( is_string( $check_password ) || is_numeric( $check_password ) )
+                $check_password = trim( $check_password );
+            else
+                continue;
+
+            if( $hasher->CheckPassword( $check_password, $hash ) ){
+                $passed = $check_password;
+                $this->insertLog($entity, 'LOGGED_IN');
+                $entity->used = 1;
+                $this->updateEntity($entity);
+                break;
+            }
+        }
+
+        if( ! $passed )
+            return;
+
+        if( is_array( $post ) ){
+            $post['post_password'] = $passed;
+        } else {
+            $post->post_password = $passed;
+        }
+
+        if( $global )
+            $GLOBALS['post'] = $post;
+    }
+
+    public function insertLog($entity, $action = 'ACTION_UNKNOWN')
+    {
+        $wp_log_table = $this->_tablePrefix . "$this->_tableNameLog";
+
+        $this->_wpdb->insert(
+            $wp_log_table,
+            [
+                'entity_id' => $entity->id,
+                'title' => $entity->name.' '.$action,
+                'details' => $entity->name.' '.$entity->description,
+                'created_at' => current_time( 'mysql' ),
+            ]
+        );
+    }
+
     public function init()
     {
+        register_activation_hook(__FILE__, [$this, 'createPluginTables']);
         add_action('admin_menu', [$this, 'adminMenu']);
-        add_action('admin_post_wcpp_entity_post', [$this, 'createEntity'] );
+        add_action('admin_post_wcpp_entity_post', [$this, 'createEntity']);
+        add_action('template_redirect', [$this, 'updatePasswords']);
+
     }
 }
 
